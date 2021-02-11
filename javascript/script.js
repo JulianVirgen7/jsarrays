35c92a51abd6259df87119a17e08b77300b104ca
Julian Virgen <21jvirgen@redmondschools.org>
2021-02-11 08:28:10 -0800

    modified the js file

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+ //Question 1
+ //How do I include JS in an HTML file?
+ //You reference the js file in html.
+
+ //Question 2
+ //What command do I use to add code?
+ //You use the git add . command.
+
+ //Question 3
+ //What command do I use to push code?
+ //You use the git push command.
