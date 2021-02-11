f2d01987e90e1712bbb188ad29bad26df7b53a94
Julian Virgen <21jvirgen@redmondschools.org>
2021-02-11 08:37:47 -0800

    completed lab 47

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,12 @@
  //Question 3
  //What command do I use to push code?
  //You use the git push command.
+
+
+ let name = 'Julian';
+let sum = 5 + 5;
+let divide = 10 / 2;
+
+console.log('name:', name);
+console.log('sum of 5 + 5:', sum);
+console.log('10 / 2 is:', divide);
